c9af9b72b0c69e35b466b5d852cffd7ee71f2416
lahirutm <lahirutm@gmail.com>
2023-01-30 10:02:56 +0530

    missing file close added after file create

--- ESP32_CODE/esp-32-web-server-with-spiff-filesystem-1.ino
+++ ESP32_CODE/esp-32-web-server-with-spiff-filesystem-1.ino
@@ -2,13 +2,10 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebSrv.h>
 #include <SPIFFS.h>
-#include <ArduinoJson.h>
-#include <EEPROM.h>
 
 // Replace with desired credentials for your ESP32 Access Point
 const char* ssid     = "ESP32-Access-Point";
 const char* password = "123456789";
-
 IPAddress local_ip(192,168,4,1);
 IPAddress gateway(192,168,4,1);
 IPAddress subnet(255,255,255,0);
@@ -23,8 +20,7 @@ const char index_html[] PROGMEM = R"rawliteral(
   <meta charset="UTF-8">
 </head>
 <body>
-  <p>Free Storage: %FREESPIFFS% | Used Storage: %USEDSPIFFS% | Total Storage: %TOTALSPIFFS%</p>
-  <p>%FILELIST%</p>
+  <p>&nbsp;</p>
 </body>
 </html>
 )rawliteral";
@@ -32,12 +28,10 @@ const char index_html[] PROGMEM = R"rawliteral(
 
 AsyncWebServer *server;               // initialise webserver
 File config_json;
-const char *fileName = "/config.json";
 
 void setup() {
   Serial.begin(115200);
-  delay(500);
-  EEPROM.begin(2048);
+
   Serial.println("Booting ...");
 
   Serial.println("Mounting SPIFFS ...");
@@ -48,19 +42,19 @@ void setup() {
     rebootESP("ERROR: Cannot mount SPIFFS, Rebooting");
   }
 
-  //Serial.print("SPIFFS Free: "); Serial.println(humanReadableSize((SPIFFS.totalBytes() - SPIFFS.usedBytes())));
-  //Serial.print("SPIFFS Used: "); Serial.println(humanReadableSize(SPIFFS.usedBytes()));
-  //Serial.print("SPIFFS Total: "); Serial.println(humanReadableSize(SPIFFS.totalBytes()));
+  Serial.print("SPIFFS Free: "); Serial.println(humanReadableSize((SPIFFS.totalBytes() - SPIFFS.usedBytes())));
+  Serial.print("SPIFFS Used: "); Serial.println(humanReadableSize(SPIFFS.usedBytes()));
+  Serial.print("SPIFFS Total: "); Serial.println(humanReadableSize(SPIFFS.totalBytes()));
 
   Serial.println(listFiles());
 
   Serial.println("\nLoading Configuration ...");
-  
+
   Serial.print("Setting AP (Access Point)…");
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(local_ip, gateway, subnet);
   WiFi.softAP(ssid, password);
-  
+
   Serial.print("AP IP address: ");
   Serial.println(WiFi.softAPIP());
 
@@ -140,34 +134,30 @@ void configureWebServer() {
 
 void handleBody(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){
  
-  //String logmessage = "Writing data file size: " + String(total);
+  const char *fileName = "/config.json";
+  String logmessage = "Writing data file size: " + String(total);
 
   if(!index){
     if (SPIFFS.exists(fileName)) {
       SPIFFS.remove(fileName);
     }
-    //logmessage += "Write Start: " + String(fileName);
-    config_json = SPIFFS.open(fileName, "w");
-    //Serial.println(logmessage);
+    logmessage += "Write Start: " + String(fileName);
+    config_json = SPIFFS.open(fileName, "a");
+    Serial.println(logmessage);
   }
   if (len) {
     config_json.write(data, len);
-    //logmessage = "Writing file: " + String(fileName) + " index=" + String(index) + " len=" + String(len);
-    //Serial.println(logmessage);
+    logmessage = "Writing file: " + String(fileName) + " index=" + String(index) + " len=" + String(len);
+    Serial.println(logmessage);
   }
   if(index + len == total){
-    //logmessage = "Write Complete: " + String(fileName) + ",size: " + String(index + len);
+    logmessage = "Write Complete: " + String(fileName) + ",size: " + String(index + len);
     config_json.close();
-    //Serial.println(logmessage);
-    save_to_eeprom();
+    Serial.println(logmessage);
   }
 }
 
 String processor(const String& var) {
-  if (var == "FILELIST") {
-    return listFiles();
-  }
-  
   if (var == "FREESPIFFS") {
     return humanReadableSize((SPIFFS.totalBytes() - SPIFFS.usedBytes()));
   }
@@ -181,59 +171,4 @@ String processor(const String& var) {
   }
 
   return String();
-}
-
-
-void save_to_eeprom(){
-  File saved_json = SPIFFS.open(fileName, "r");
-  Serial.println("Read from stored JSON");
-  DynamicJsonDocument doc(81920);
-  deserializeJson(doc, saved_json);
-
-  for (JsonArray array1 : doc.as<JsonArray>())
-  {
-      int array_size = array1.size();
-      if(array_size == 5){
-        for(JsonArray array2:array1)
-        {          
-            for(JsonVariant v : array2)
-            {
-              int addr = v["addr"];
-              String value = v["value"];
-              
-              if(!isDigit(value.charAt(0))){       // Text Label
-                char text_label[10];
-                strcpy(text_label, value.c_str());
-                EEPROM.put(addr, text_label);
-              }
-              else {                              // Numeric Value
-                int int_value = v["value"];
-                EEPROM.write(addr, int_value );
-              }
-              //Serial.println(addr);
-            }
-        }
-      }
-
-      if(array_size == 4){
-        for(JsonVariant v : array1){
-          int addr = v["addr"];
-          String value = v["value"];
-                
-          if(!isDigit(value.charAt(0))){       // Text Label
-            char text_label[10];
-            strcpy(text_label, value.c_str());
-            EEPROM.put(addr, text_label);
-          }
-          else {                              // Numeric Value
-            int int_value = v["value"];
-            EEPROM.write(addr, int_value );
-          }
-          //Serial.println(addr);
-        }
-      }
-  }
-  saved_json.close();
-  EEPROM.commit();
-  //Serial.println("COMMIT TO EEPROM");
 }
